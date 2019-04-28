8d6d3536a6e9fbc89bdd5908fdb31f2ecbcc8f96
Together <together@google.com>
2019-04-28 12:07:00 +0200

    add game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject Player_1;
+    public GameObject Player_2;
+
+    private bool First_Player_Turn;
+    private Vector3 Throw_Vector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        First_Player_Turn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject Current_Player;
+        if (Input.GetMouseButtonDown(0)) {
+            Throw_Vector = Input.mousePosition;
+        } else if (Input.GetMouseButtonUp(0)) {
+            if (First_Player_Turn) {
+                Current_Player = Player_1;
+            } else {
+                Current_Player = Player_2;
+            }
+
+            Throw_Vector = Input.mousePosition - Throw_Vector;
+            Current_Player.GetComponent<PongBehaviour>().Throw_Pong(Throw_Vector);
+
+            First_Player_Turn = !First_Player_Turn;
+        }
+    }
+}
--- Assets/Scripts/PongBehaviour.cs
+++ Assets/Scripts/PongBehaviour.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PongBehaviour : MonoBehaviour {
 
-	public GameObject Player_1_Emitter;
+	public GameObject Player_Emitter;
 	public GameObject Pong;
 	public float Pong_Forward_Force;
+    public bool Is_First_Player;
 
 	// Use this for initialization
 	void Start () {
@@ -15,15 +16,27 @@ public class PongBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
-			GameObject Temporary_Bullet_Handler;
-			Temporary_Bullet_Handler = Instantiate (Pong, Player_1_Emitter.transform.position, Player_1_Emitter.transform.rotation) as GameObject;
-			Temporary_Bullet_Handler.transform.Rotate (Vector3.left * 90);
-			Rigidbody Temporary_RigidBody;
-			Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
-			Temporary_RigidBody.AddForce(transform.forward * Pong_Forward_Force);
-			//Temporary_RigidBody.AddForce(0f, 1f, 1f);
-			Destroy (Temporary_Bullet_Handler, 10.0f);
-		}
+        
 	}
+
+    public void Throw_Pong(Vector3 Throw_Vector) {
+        Throw_Vector.x = Throw_Vector.x * -1 * Get_Player_Factor();
+        Throw_Vector.z = (Throw_Vector.y * -1) * Pong_Forward_Force * Get_Player_Factor();
+        Throw_Vector.y = 20f;
+
+        GameObject Temporary_Bullet_Handler;
+        Temporary_Bullet_Handler = Instantiate(Pong, Player_Emitter.transform.position, Player_Emitter.transform.rotation) as GameObject;
+        Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+        Rigidbody Temporary_RigidBody;
+        Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+        Temporary_RigidBody.AddForce(Throw_Vector);
+        Destroy(Temporary_Bullet_Handler, 10.0f);
+    }
+
+    int Get_Player_Factor() {
+        if (Is_First_Player) {
+            return 1;
+        }
+        return -1;
+    }
 }
