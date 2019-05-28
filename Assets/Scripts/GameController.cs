68d2d57bd54a5667c252bac5df3513e219a4ce93
Together <together@google.com>
2019-05-28 18:29:58 +0200

    add foam cups

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class GameController : MonoBehaviour
 {
     public GameObject Player_1;
     public GameObject Player_2;
+    public GameObject FoamCup;
     public Camera Main_Camera;
 
     private bool First_Player_Turn;
@@ -22,6 +24,9 @@ public class GameController : MonoBehaviour
         First_Player_Turn = true;
         Pong_Exists = false;
         Other_Player = Player_1;
+        Main_Camera.transform.position = Player_1.transform.position;
+        Main_Camera.transform.rotation = Player_1.transform.rotation;
+        setCups();
     }
 
     //public void ShakeIt()
@@ -44,6 +49,7 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update() {
         //ShakeIt();
+        Handle_Keyboard();
         if (Pong_Exists)
         {
             Change_Player_Turn();
@@ -53,6 +59,17 @@ public class GameController : MonoBehaviour
             Handle_Camera_Change();
             Handle_Pong_Throw();
         }
+    }
+
+    private void Handle_Keyboard()
+    {
+        if (Input.GetKeyDown("r")) {
+            restartGame();
+        }
+    }
+
+    public void restartGame() {
+        Start();
     }
 
     private void Handle_Pong_Throw() {
@@ -74,8 +91,8 @@ public class GameController : MonoBehaviour
     }
 
     private void Handle_Camera_Change() {
-        Main_Camera.transform.position = Vector3.Lerp(Main_Camera.transform.position, Other_Player.transform.position, 0.02f);
-        Main_Camera.transform.rotation = Quaternion.Slerp(Main_Camera.transform.rotation, Other_Player.transform.rotation, 0.02f);
+        Main_Camera.transform.position = Vector3.Lerp(Main_Camera.transform.position, Other_Player.transform.position, 0.04f);
+        Main_Camera.transform.rotation = Quaternion.Slerp(Main_Camera.transform.rotation, Other_Player.transform.rotation, 0.04f);
     }
 
     private void Change_Player_Turn()
@@ -92,7 +109,34 @@ public class GameController : MonoBehaviour
             }
             First_Player_Turn = !First_Player_Turn;
 
+            var foamCup = GameObject.FindGameObjectsWithTag("FoamCup");
+            foreach (var obj in foamCup)
+            {
+                obj.transform.rotation = new Quaternion(0, 90, 90, 0);
+            }
+
             Pong_Exists = false;
         }
+    }
+
+    private void setCups() {
+        var foamCup = GameObject.FindGameObjectsWithTag("FoamCup");
+        foreach (var obj in foamCup) {
+            Destroy(obj);
+        }
+
+        Instantiate(FoamCup, new Vector3(0f, 10f, 1.25f), new Quaternion(0, 90, 90, 0));
+        Instantiate(FoamCup, new Vector3(0.5f, 10f, 2.25f), new Quaternion(0, 90, 90, 0));
+        Instantiate(FoamCup, new Vector3(-0.5f, 10f, 2.25f), new Quaternion(0, 90, 90, 0));
+        Instantiate(FoamCup, new Vector3(1f, 10f, 3f), new Quaternion(0, 90, 90, 0));
+        Instantiate(FoamCup, new Vector3(-1f, 10f, 3f), new Quaternion(0, 90, 90, 0));
+        Instantiate(FoamCup, new Vector3(0f, 10f, 3f), new Quaternion(0, 90, 90, 0));
+
+        Instantiate(FoamCup, new Vector3(0f, 10f, -1.25f), new Quaternion(0, 90, 90, 0));
+        Instantiate(FoamCup, new Vector3(0.5f, 10f, -2.25f), new Quaternion(0, 90, 90, 0));
+        Instantiate(FoamCup, new Vector3(-0.5f, 10f, -2.25f), new Quaternion(0, 90, 90, 0));
+        Instantiate(FoamCup, new Vector3(1f, 10f, -3f), new Quaternion(0, 90, 90, 0));
+        Instantiate(FoamCup, new Vector3(-1f, 10f, -3f), new Quaternion(0, 90, 90, 0));
+        Instantiate(FoamCup, new Vector3(0f, 10f, -3f), new Quaternion(0, 90, 90, 0));
     }
 }
